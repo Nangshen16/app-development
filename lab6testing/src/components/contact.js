92940664748531b5cdcfb626d6404139a812d23f
NSH <nshnotafanofrats@NSHs-MBP.attlocal.net>
2024-07-18 21:07:45 -0400

    add contact.js

---
+++ lab6testing/src/components/contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Contact = function(){
+    return(
+        <div>
+            <header className='header'>
+                <h1 className='headertitle'>
+                    Contact Me
+                </h1>
+            </header>
+        </div>
+    )
+}
+
+export default Contact;
